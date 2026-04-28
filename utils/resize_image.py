3231b341d3d5f5946b6fee8bd0fb0b8d8dd157df
infowarfare <infowarfare@web.de>
2026-04-28 14:58:05 +0200

    fix: docstring changes in resize_image.py

--- utils/resize_image.py
+++ utils/resize_image.py
@@ -3,7 +3,7 @@ import io
 from PIL import Image
 
 def resize_image(base64_image: str, max_size: int = 512) -> str:
-    """Bild auf max_size px verkleinern und als Base64 zurückgeben."""
+    """Reduce image size (and possibly image quality) and return as Base64 encoded string."""
     image_bytes = base64.b64decode(base64_image)
     img = Image.open(io.BytesIO(image_bytes))
     img.thumbnail((max_size, max_size))  # behält Seitenverhältnis
