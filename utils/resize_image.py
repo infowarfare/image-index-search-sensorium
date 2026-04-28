94ab872a7ce8b7e47a515e53e9b1f5bf73d19522
infowarfare <infowarfare@web.de>
2026-04-28 16:40:04 +0200

    fix: added quality param to resize_image function

--- utils/resize_image.py
+++ utils/resize_image.py
@@ -2,11 +2,11 @@ import base64
 import io
 from PIL import Image
 
-def resize_image(base64_image: str, max_size: int = 512) -> str:
+def resize_image(base64_image: str, max_size: int = 512, quality: int = 85) -> str:
     """Reduce image size (and possibly image quality) and return as Base64 encoded string."""
     image_bytes = base64.b64decode(base64_image)
     img = Image.open(io.BytesIO(image_bytes))
     img.thumbnail((max_size, max_size))  # behält Seitenverhältnis
     buffer = io.BytesIO()
-    img.save(buffer, format="JPEG", quality=85)
+    img.save(buffer, format="JPEG", quality=quality)
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
